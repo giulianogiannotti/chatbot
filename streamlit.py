92e4ca7800537c030425a18e9b2dc445932fd799
Giuliano Giannotti <71414041+giulianogiannotti@users.noreply.github.com>
2025-09-05 17:42:44 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -79,7 +79,7 @@ def load_vectorstore():
         for doc_list in results:
             documents.extend(doc_list)
 
-    splitter_small = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitter_small = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
     splits_small = splitter_small.split_documents(documents)
     for chunk in splits_small:
         res = chunk.metadata.get("resolucion", "Sin identificador")
@@ -97,7 +97,7 @@ def load_vectorstore():
 vectorstore = load_vectorstore()
 if vectorstore is None:
     st.stop()
-retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 5})
+retriever = vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 3})
 
 token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
 # --- Inicializar LLM y QA chain ---
